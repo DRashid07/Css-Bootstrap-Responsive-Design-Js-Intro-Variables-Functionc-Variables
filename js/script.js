5226208f48006c4c060dbe9d43b874dcab1e6884
mzehra213 <rashided@code.edu.az>
2026-02-24 19:46:05 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+//alert()
+// console.log("Hello World")
+// prompt("What is your name?")
+// console.log(document);
+// document.writeln("Hello World")
+
+
+// var num=14;
+// console.log(num%3 == 0 && num%7 == 0);
+
+// var n=10;
+// var m=20;
+// var sum=0;
+// for (;n<m;n++){
+//     if (n%2 != 0){
+//         sum=sum+n;
+//     }
+// }
+// console.log(sum);
+
+// var n=10;
+// var m=20;
+// var sum=0;
+// for (;n<m;n++){
+//     if (n%2 != 0){
+//         sum++;
+//     }
+// }
+// console.log(sum);
+
+// var num=10;
+// var a=0;
+// for (n=1;n<num;n++){
+//     for (m=1;m<num;m++){
+//         if (num%n == 0 && num%m == 0){
+//             a++;
+//         }
+//     }
+// }
+// if (a > 1){
+//     console.log(num + " murekkeb ededdir");
+// }
+// else{
+//     console.log(num + " sade ededdir");
+// }
+
+// var arr=[1,2,3,4,5];
+// var sum=0;
+// for (var i=0;i<arr.length;i++){
+//     if (arr[i]%2 == 0){
+//         sum=sum+arr[i];
+//     }
+// }
+// console.log(sum);
+
+
